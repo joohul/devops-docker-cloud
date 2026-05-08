0ca152562c4ced858a2e2d327828aa34f7b77d8e
Joona Hulkkonen (TAU) <joona.e.hulkkonen@tuni.fi>
2026-05-08 19:54:06 +0300

    Update page text

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from a Web App!</h1>')
+  res.send('<h1>Hello from a Web Application!</h1>')
 })
 
 const PORT = 80
